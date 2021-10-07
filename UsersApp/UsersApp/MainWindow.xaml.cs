680e1871356dc949e9f20285df201842e39bd974
CsharpL <igr0ks@ya.ru>
2021-10-08 02:49:44 +0300

    обработка данных

--- UsersApp/UsersApp/MainWindow.xaml.cs
+++ UsersApp/UsersApp/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace UsersApp
         {
             InitializeComponent();
         }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass2 = passBox2.Password.Trim(); 
+            string email = textBoxEmail.Text.Trim().ToLower();
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Логин должен быть больше 5 символов";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if(pass.Length < 5)
+            { 
+                passBox.ToolTip = "пароль должен быть больше 5 символов";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass2)
+            {
+                passBox2.ToolTip = "пароли должны совпадать";
+                passBox2.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "некорректная почта";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox2.ToolTip = "";
+                passBox2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+            MessageBox.Show("Все ок!");
+
+            }
+
+        }
     }
 }
